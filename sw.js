db484c735424e5f4860b26a39f4ec5921f328367
FNY666 <90173163+FNY666@users.noreply.github.com>
2026-09-24 00:14:31 +0800

    refactor: 缓存版本号 v2->v3，同步 game.js?v44

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 // 像素乱斗 Service Worker：安装后可离线游玩
 'use strict';
 
-const VERSION = 'pixel-brawl-v2';
+const VERSION = 'pixel-brawl-v3';
 const CACHE_NAME = VERSION;
 // 注意：必须与 index.html 中的资源版本号一致
 const ASSETS = [
   './',
   './index.html',
   './style.css?v=11',
-  './game.js?v=43',
+  './game.js?v=44',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
